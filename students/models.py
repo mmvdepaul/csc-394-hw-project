a86a2b8bf6bf9ad85d2bd1fd847e80d9ad789d30
Marcos Velazquez <mvelaz24@depaul.edu>
2023-01-27 13:06:23 -0600

    Added User field for Student database

--- students/models.py
+++ students/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,6 +7,7 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, primary_key=False)
 
     def __str__(self) -> str:
         return self.first_name + " " + self.last_name
@@ -15,6 +17,6 @@ class Course(models.Model):
     department = models.CharField(max_length=60)
     number = models.CharField(max_length=3)
     students = models.ManyToManyField(Student)
-    
+
     def __str__(self) -> str:
         return self.department + "-" + self.number
